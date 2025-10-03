c965836570efdfadb5fa1489b083aa79631879e7
NomanAijaz <nomikk08@gmail.com>
2025-10-04 02:48:38 +0500

    added bookings page

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Booking
 
-# Create your views here.
+@login_required
+def bookings_page(request):
+    # Admin/staff can see all bookings
+    if request.user.is_staff:
+        bookings = Booking.objects.select_related("buyer", "plot", "plan").prefetch_related("payments")
+    else:
+        bookings = Booking.objects.filter(buyer=request.user).select_related("plot", "plan").prefetch_related("payments")
+
+    return render(request, "bookings/bookings_page.html", {"bookings": bookings})

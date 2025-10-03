446fd88506e746bbc5afc810e1a8bdcb75438bed
NomanAijaz <nomikk08@gmail.com>
2025-10-04 02:44:08 +0500

    Added plots page

--- plots/views.py
+++ plots/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Plot, InstallmentPlan
 
-# Create your views here.
+def plot_list(request):
+    plots = Plot.objects.all().select_related()
+    plans = InstallmentPlan.objects.all()
+
+    plan_id = request.GET.get("plan")
+    status = request.GET.get("status")
+
+    if plan_id:
+        plots = plots.filter(booking__plan_id=plan_id)
+    if status:
+        plots = plots.filter(status=status)
+
+    context = {
+        "plots": plots,
+        "plans": plans,
+        "selected_plan": plan_id,
+        "selected_status": status,
+    }
+    return render(request, "plots/plot_list.html", context)

f26ea056f7bb218c6ba8024caf7e90096861621c
NomanAijaz <nomikk08@gmail.com>
2025-10-04 11:21:31 +0500

    added home page

---
+++ installments/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from plots.models import Plot
+
+def home(request):
+    total_plots = Plot.objects.count()
+    available = Plot.objects.filter(status="available").count()
+    reserved = Plot.objects.filter(status="reserved").count()
+    sold = Plot.objects.filter(status="sold").count()
+
+    featured_plots = Plot.objects.filter(status="available")[:6]  # show 6 available
+
+    context = {
+        "total_plots": total_plots,
+        "available": available,
+        "reserved": reserved,
+        "sold": sold,
+        "featured_plots": featured_plots,
+    }
+    return render(request, "home.html", context)

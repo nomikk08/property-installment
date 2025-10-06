6a3baeafea2b224946ba09533d475a4c2180a7d2
NomanAijaz <nomikk08@gmail.com>
2025-10-06 17:34:02 +0500

    added commission field

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,17 +1,29 @@
 from django.contrib import admin
 from .models import Booking, Payment
 
+
 class PaymentInline(admin.TabularInline):
     model = Payment
     extra = 0
     readonly_fields = ("due_date", "amount", "is_paid")
 
+
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ("buyer", "plot", "plan", "down_payment_amount", "monthly_installment", "start_date", "is_completed")
+    list_display = (
+        "buyer",
+        "plot",
+        "plan",
+        "down_payment_amount",
+        "monthly_installment",
+        "start_date",
+        "is_completed",
+    )
     list_filter = ("is_completed", "plan")
     search_fields = ("buyer__username", "plot__title")
+    readonly_fields = ("total_paid_amount",)
     inlines = [PaymentInline]
+
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
--- bookings/models.py
+++ bookings/models.py
@@ -2,23 +2,42 @@ from django.db import models
 
 from accounts.models import Buyer
 from plots.models import InstallmentPlan, Plot
+from django.db.models import Sum
 
-# Create your models here.
 
 class Booking(models.Model):
     buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, related_name="bookings")
-    plot = models.OneToOneField(Plot, on_delete=models.CASCADE)  # One plot → One booking
+    plot = models.OneToOneField(
+        Plot, on_delete=models.CASCADE
+    )  # One plot → One booking
     plan = models.ForeignKey(InstallmentPlan, on_delete=models.CASCADE)
     down_payment_amount = models.DecimalField(max_digits=12, decimal_places=2)
     monthly_installment = models.DecimalField(max_digits=12, decimal_places=2)
+    commission_paid = models.DecimalField(
+        blank=True,
+        null=True,
+        max_digits=12,
+        decimal_places=2,
+        verbose_name="Commission Paid",
+    )
     start_date = models.DateField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.buyer.username} - {self.plot.title}"
-    
+
+    @property
+    def total_paid_amount(self):
+        total = self.payments.filer(is_paid=True).aggregate(
+            total=Sum("amount")["total"]
+        )
+        return total or 0
+
+
 class Payment(models.Model):
-    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name="payments")
+    booking = models.ForeignKey(
+        Booking, on_delete=models.CASCADE, related_name="payments"
+    )
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     due_date = models.DateField()
     paid_date = models.DateField(blank=True, null=True)
@@ -26,4 +45,3 @@ class Payment(models.Model):
 
     def __str__(self):
         return f"{self.booking.plot.title} - {self.amount} - {'Paid' if self.is_paid else 'Pending'}"
-

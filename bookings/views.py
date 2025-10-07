0789d0d04032995dc51a2db8a9bfd9d994447e98
NomanAijaz <nomikk08@gmail.com>
2025-10-07 12:03:55 +0500

    fix: booking view

--- bookings/views.py
+++ bookings/views.py
@@ -3,13 +3,10 @@ from django.contrib.auth.decorators import login_required
 from .models import Booking
 
 
-# @login_required
+@login_required
 def bookings_page(request):
-
-    bookings = (
-        Booking.objects.filter(buyer=request.user)
-        .select_related("plot", "installment_months")
-        .prefetch_related("payments")
+    bookings = Booking.objects.select_related("buyer", "plot").prefetch_related(
+        "payments"
     )
 
     return render(request, "bookings/bookings_page.html", {"bookings": bookings})

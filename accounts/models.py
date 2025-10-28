31a3b77c01f76b2dfe94248929eade2d41b6f696
Noman Aijaz <noman@safetyeq.ai>
2025-10-28 21:24:11 +0500

    removed validators

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.validators import RegexValidator
 
 
 class User(AbstractUser):
@@ -14,31 +13,17 @@ class Buyer(models.Model):
     name = models.CharField(max_length=100, verbose_name="Name")
     father_name = models.CharField(max_length=100, verbose_name="Father's Name")
     contact_no = models.CharField(
-        max_length=11,
-        validators=[
-            RegexValidator(
-                regex=r"^\d{11}$", message="Contact number must be exactly 11 digits."
-            )
-        ],
         verbose_name="Phone Number",
     )
     cnic = models.CharField(
         max_length=16,
         unique=True,
-        validators=[
-            RegexValidator(regex=r"^\d{16}$", message="CNIC must be exactly 16 digits.")
-        ],
         verbose_name="CNIC Number",
     )
     address = models.TextField(verbose_name="Address")
     inheritor = models.CharField(max_length=100, verbose_name="Inheritor's Name")
     inheritor_cnic = models.CharField(
         max_length=16,
-        validators=[
-            RegexValidator(
-                regex=r"^\d{16}$", message="Inheritor CNIC must be exactly 16 digits."
-            )
-        ],
         verbose_name="Inheritor's CNIC",
     )
     inheritor_relation = models.CharField(
